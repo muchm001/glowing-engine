a5289552c3d4de6efa9b12708681f0532dd5c0d5
muchm001 <muchm001@gmail.com>
2018-02-15 11:07:44 -0600

    Add files via upload

---
+++ NENHome.js
@@ -0,0 +1,28 @@
+var imput = <div class="jumbotron">
+  <div class="container">
+      <h1 class='display-3'>New England Nonwoven</h1>
+      <hr class='m-y-2'>
+      <p>Needlepunch Nonwovens for:
+Laminating & Coating • Filtration • Health Care • Home Products • Molding</p>
+      <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+  </div>
+</div>
+  
+    
+    <div class="container-fluid" id="bodyText">
+        <div class='row' id='companySummary'>
+            <p>
+                New England Nonwovens (formally the Deitsch Plastic non-woven division) is a leading manufacturer of engineered needlepunch nonwoven fabrics for a variety of markets and applications. Purchased in 2009 by long time industry executives Mike Hochlowski and John Uchmanowicz, New England Nonwovens has some of the most technically advanced machinery in North America. The company draws on an experienced work force, up to date equipment and broad based knowledge of its marketing, operations and technical support to create innovative solutions for customer applications. 
+            </p>
+            <p>
+                New England Nonwovens draws on 30 years of manufacturing expertise and technical experience to provide innovative solutions for our customer's needlepunch fabric end uses. NEN has developed and produced needlepunch nonwoven fabrics for uses in health care, filtration, home crafts, bedding, CIPP pipe linings and composite building markets.
+            </p>
+            <p>
+                The strategic mission of NEN is to provide, innovative, superior quality needlepunch fabrics with excellent service and production flexibility to meet our customer's needs. New England Nonwovens works hand-in-hand with our customers to ensure the needlepunch fabric developed and produced meets the needs of the application. 
+            </p>
+            <p>
+                Situated in a sizable manufacturing facility in West Haven CT with room to expand, New England Nonwovens anticipates steady growth over the next 5 years. NEN is looking forward to working with you to provide cost effective solutions to your needlepunch nonwoven requirements.
+            
+            </p>
+    </div>'
+      
